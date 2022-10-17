164f1593ac293a73fb5883a938099ed2c6e6c943
akashkuchankar <kuchankarakash@gmail.com>
2022-10-17 00:27:16 -0700

    Add project files.

---
+++ ConsoleApp12/Class1.cs
@@ -0,0 +1,25 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConsoleApp12
+//{
+//    internal class Class1
+//    {
+//        static void Main(string[] args)
+//        {
+//            string[]names=new string[5];
+//            for(int i=0; i<names.Length; i++)
+//            {
+//                names[i]=Console.ReadLine();
+//            }  
+//            for(int i=0; i<names.Length; i++)
+//            {
+//                Console.WriteLine($"names[{i}]={names[i]}");
+//            }
+            
+//        }
+//    }
+//}
---
+++ ConsoleApp12/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            double[] rainfall = new double[7];
+            for(int i = 0; i <rainfall.Length; i++)  
+            {
+                rainfall[i]=Convert.ToDouble(Console.ReadLine());
+
+            }
+            for(int i=0; i < rainfall.Length; i++)
+            {
+                Console.WriteLine($"rainfall[{i}]={rainfall[i]}"); 
+            }
+          
+        }
+    }
+}
