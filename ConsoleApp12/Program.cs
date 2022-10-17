7cb0dcd99179cf720a6b9e12b86b76b8940b8a64
akashkuchankar <kuchankarakash@gmail.com>
2022-10-17 00:28:42 -0700

    aray basic

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,27 +1,27 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace ConsoleApp12
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int[] number = new int[5];
-//            for(int i = 0; i < number.Length; i++)
-//            {
-//                number[i] = Convert.ToInt32(Console.ReadLine());
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[5];
+            for (int i = 0; i < number.Length; i++)
+            {
+                number[i] = Convert.ToInt32(Console.ReadLine());
 
-//            }
-//            for (int i = 0; i < number.Length; i++)
-//            {
-//                Console.WriteLine($"number[{i}]={number[i]}");
-//            }
-              
-            
-//        }
-//    }
-//}
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine($"number[{i}]={number[i]}");
+            }
+
+
+        }
+    }
+}
